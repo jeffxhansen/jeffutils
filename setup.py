b5001112e752200c9b44825275d0d74590dd5261
Jeff Hansen <jeffxhansen@gmail.com>
2024-05-31 11:22:35 -0600

    finished testing automatic gitpush

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='jeffutils',
-    version='0.7.7',
+    version='0.7.8',
     author='Jeff Hansen',
     author_email='jeffxhansen@gmail.com',
     description="Welcome to Jeff Hansen's suite of useful python functions! I use lots of these functions on most of my Data Analysis, Backend-Dev, and Machine Learning projects, and I hope you find some of them useful as well!",
